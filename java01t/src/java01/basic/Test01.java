3bccc5af9e7114a5bbe9b54c878756ebeedced8d
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-12-10 12:09:19 +0900

    .

---
+++ java01t/src/java01/basic/Test01.java
@@ -0,0 +1,26 @@
+package java01.basic;
+
+//1. 변수 선언 : 데이터 저장소 준비
+public class Test01 {
+	public static void main(String[] args) {
+		// [데이터 종류와 크기] [메모리 별명];
+		// 자바의 기본 데이터 종류: 정수, 실수, 논리, 문자
+		// 1) 정수: byte(1), short(2), int(4), long(8)
+		// 2) 실수: float(4), double(8)
+		// 3) 논리: boolean(1)
+		// 4) 문자: char(2)
+		
+		byte b1; // 변수 선언
+		b1 = 20; // 값 할당
+		
+		b1 = -128;
+		
+		byte b2, b3, b4;
+		
+		byte b5 = 20;
+		
+		byte b6=20, b7=30;
+		
+	}
+
+}
