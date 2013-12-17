1391930ad9806288b690ea3baf2c01ddc4014a69
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-12-17 11:17:09 +0900

    .

--- java01t/src/java01/exam07/test/Test05.java
+++ java01t/src/java01/exam07/test/Test05.java
@@ -1,9 +1,54 @@
 package java01.exam07.test;
 
+import java.util.HashMap;
+
 import java01.exam07.vo.Member;
 
 public class Test05 {
 	public static void main(String[] args) {
+		Member m1 = new Member();
+		m1.setName("홍길동");
+		m1.setAge(20);
+		m1.setEmail("hong@test.com");
+		m1.setTel("111-1111");
+		
+		Member m2 = new Member();
+		m2.setName("홍길동");
+		m2.setAge(20);
+		m2.setEmail("hong@test.com");
+		m2.setTel("111-1111");
+		
+		String s1 = new String("1010");
+		String s2 = new String("1010");
+		
+		System.out.println(m1.hashCode());
+		System.out.println(m2.hashCode());
+		
+		HashMap map = new HashMap();
+		map.put(m1, "1000000원");
+		
+		System.out.println(map.get(m2));
+		
+	}
+	
+	public static void main02(String[] args) {
+		Member m1 = new Member();
+		m1.setName("홍길동");
+		m1.setAge(20);
+		m1.setEmail("hong@test.com");
+		m1.setTel("111-1111");
+		
+		Member m2 = new Member();
+		m2.setName("홍길동");
+		m2.setAge(20);
+		m2.setEmail("hong@test.com");
+		m2.setTel("111-1111");
+		
+		System.out.println(m1.hashCode());
+		System.out.println(m2.hashCode());
+	}
+	
+	public static void main01(String[] args) {
 		Member m1 = new Member();
 		m1.setName("홍길동");
 		m1.setAge(20);
@@ -22,6 +67,8 @@ public class Test05 {
 		if (m1.equals(m2))
 			System.out.println("m1.equals(m2)");
 		
+		System.out.println(m1);
+		System.out.println(m2);
 	}
 }
 
--- java01t/src/java01/exam07/vo/Member.java
+++ java01t/src/java01/exam07/vo/Member.java
@@ -1,6 +1,14 @@
 package java01.exam07.vo;
 
-// 3. equals() 재정의 
+// 3. Object 클래스의 메서드 재정의 
+// - equals()
+// - toString()
+// - finalize() : 가비지 컬렉터가 객체를 해제하기 전에 호출하는 메서드
+//						가비지 컬렉터 언제 호출하는지 보장 못함.
+// - hashCode() : 인스턴스 마다 고유의 정수 값 반환
+//				같은 값을 가지는 객체는 같은 해시코드를 리턴하게 해야 한다.
+//				해시 값은 인스턴스를 구분하는 값
+//				예) 공인인증서 코드
 public class Member /*extends Object*/ {
 	protected String name;
 	protected int age;
@@ -8,22 +16,51 @@ public class Member /*extends Object*/ {
 	protected String tel;
 	
 	@Override
-	public boolean equals(Object obj) {
-		if (obj == null || !(obj instanceof Member)) 
-			return false;
-		
-		Member other = (Member)obj;
-		if (!this.name.equals(other.name))
-			return false;
-		if (this.age != other.age)
-			return false;
-		if (!this.email.equals(other.email))
-			return false;
-		if (!this.tel.equals(other.tel))
-			return false;
-		return true;
-	}
-	
+  public String toString() {
+	  return "Member [name=" + name + ", age=" + age + ", email=" + email
+	      + ", tel=" + tel + "]";
+  }
+
+	@Override
+  public int hashCode() {
+	  final int prime = 31;
+	  int result = 1;
+	  result = prime * result + age;
+	  result = prime * result + ((email == null) ? 0 : email.hashCode());
+	  result = prime * result + ((name == null) ? 0 : name.hashCode());
+	  result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+	  return result;
+  }
+
+	@Override
+  public boolean equals(Object obj) {
+	  if (this == obj)
+		  return true;
+	  if (obj == null)
+		  return false;
+	  if (getClass() != obj.getClass())
+		  return false;
+	  Member other = (Member) obj;
+	  if (age != other.age)
+		  return false;
+	  if (email == null) {
+		  if (other.email != null)
+			  return false;
+	  } else if (!email.equals(other.email))
+		  return false;
+	  if (name == null) {
+		  if (other.name != null)
+			  return false;
+	  } else if (!name.equals(other.name))
+		  return false;
+	  if (tel == null) {
+		  if (other.tel != null)
+			  return false;
+	  } else if (!tel.equals(other.tel))
+		  return false;
+	  return true;
+  }
+
 	public String getName() {
 		return name;
 	}
