f43f149ae5eaf0d2cfbec4eb1e4d2e03c9f99ac9
Jinyoung Eom <jinyoung.eom@gmail.com>
2013-12-18 09:47:34 +0900

    .

---
+++ java01t/src/java01/exam09/SpmsApp.java
@@ -0,0 +1,71 @@
+package java01.exam09;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java01.exam08.vo.Member;
+
+public class SpmsApp {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String command = null;
+		ArrayList members = new ArrayList();
+		Member member = null;
+		
+		do {
+			member = new Member();
+			
+			System.out.print("이름:");
+			member.setName(scanner.nextLine());
+			
+			System.out.print("나이:");
+			member.setAge( Integer.parseInt(scanner.nextLine()) );
+					
+			System.out.print("이메일:");
+			member.setEmail(scanner.nextLine());
+			
+			System.out.print("전화:");
+			member.setTel(scanner.nextLine());
+			
+			System.out.print("등록하시겠습니까?(y/n)");
+			command = scanner.nextLine();
+			
+			if (command.toLowerCase().equals("y")) {
+				members.add(member);
+				System.out.println("등록 성공입니다.");
+			} else {
+				System.out.println("등록 취소하였습니다.");
+			}
+			
+			System.out.print("계속하시겠습니까?(y/n)");
+			command = scanner.nextLine();
+		} while (command.toLowerCase().equals("y"));
+		
+		System.out.println("----------------------------");
+		System.out.println("이름 \t 나이 \t 전화");
+		System.out.println("----------------------------");
+		for (int i = 0; i < members.size(); i++) {
+			member = (Member)members.get(i);
+			System.out.print(member.getName() + "\t");
+			System.out.print(member.getAge() + "\t");
+			System.out.println(member.getTel());
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
