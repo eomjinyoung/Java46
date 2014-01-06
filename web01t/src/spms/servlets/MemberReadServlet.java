5c019c4e807ffb3ce2c315a1badc18e1c2d950c8
Jinyoung Eom <jinyoung.eom@gmail.com>
2014-01-06 10:32:00 +0900

    .

--- web01t/src/spms/servlets/MemberReadServlet.java
+++ web01t/src/spms/servlets/MemberReadServlet.java
@@ -2,37 +2,40 @@ package spms.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 import spms.dao.MemberDao;
 import spms.vo.Member;
 
-public class MemberListServlet extends GenericServlet {
+@WebServlet("/member/read")
+public class MemberReadServlet extends GenericServlet {
   private static final long serialVersionUID = 1L;
 
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
 	    throws ServletException, IOException {
+		//URL => http://localhost:9999/web01/member/read?no=3&name=aaa
+		int no = Integer.parseInt( request.getParameter("no") );
+		
 		response.setContentType("text/plain;charset=UTF-8");
 	  PrintWriter out = response.getWriter();
 	  
 	  try {
 		  MemberDao memberDao = new MemberDao();
-			ArrayList<Member> members = memberDao.selectList();
-			out.println("----------------------------");
-			out.println("번호 \t 이름 \t 나이 \t 전화");
-			out.println("----------------------------");
-			
-			for (Member member : members) {
-				out.print(member.getNo() + "\t");
-				out.print(member.getName() + "\t");
-				out.print(member.getAge() + "\t");
-				out.println(member.getTel());
+			Member member = memberDao.selectOne(no);
+			if (member != null) {
+				out.println("번호:" + member.getNo());
+				out.println("이름:" + member.getName());
+				out.println("이메일:" + member.getEmail());
+				out.println("전화:" + member.getTel());
+				out.println("나이:" + member.getAge());
+			} else {
+				out.println("해당 번호의 멤버가 없습니다!");
 			}
 	  } catch (Exception e) {
 	  		e.printStackTrace();
